f8deb26a20f80b54f6f0e9ad1129e05e3e896cbe
Leticia <lsgrifoni3@gmail.com>
2026-07-30 18:44:28 -0300

    Add files via upload

---
+++ dobro-triplo-raiz-simpl.py
@@ -0,0 +1,17 @@
+n = float(input('Digite o número: '))
+print('O dobro de {} vale {}. \n'
+      'O triplo de {} vale {}.\n'
+      'A raiz de {} é igual a {:.2f}'.format(n, (n * 2), n, (n * 3), n, (n ** (1 / 2))))
+#ou:
+#print('O dobro de {} vale {}. \n'
+    #'O triplo de {} vale {}.\n'
+    #'A raiz de {} é igual a {:.2f}'.format(n, (n * 2), n, (n * 3), n, pow(n, (1 / 2))))
+
+#n = float(input('Digite o número: '))
+#dobro = n * 2
+#triplo = n * 3
+#raiz = n ** (1/2)
+
+#print('O dobro de {} é: {}\n'
+#'seu triplo: {}\n'
+#'sua raiz: {:.2f} '.format(n, dobro, triplo, raiz))
